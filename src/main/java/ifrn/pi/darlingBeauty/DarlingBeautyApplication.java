3f75acc9bb4b92afac7a42e3a814e0f87a4a02b2
Célia Layne Gonçalves Miranda <20211204010008@ifrn.local>
2024-11-06 09:34:22 -0300

    Colocando os arquivos iniciais da aplicação DB

---
+++ src/main/java/ifrn/pi/darlingBeauty/DarlingBeautyApplication.java
@@ -0,0 +1,13 @@
+package ifrn.pi.darlingBeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarlingBeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarlingBeautyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ifrn/pi/darlingBeauty/DarlingBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.pi.darlingBeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarlingBeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
